ef1375c7cd8b4376d9370311cd6fbd69aecdae14
CodeBard_x88 <solo.developer29@gmail.com>
2024-03-06 20:44:14 +0500

    Lab1 and Lab2
    These two labs only covers the basics of Python.

---
+++ Lab1 01-02-2024/addtwoNums.py
@@ -0,0 +1,13 @@
+
+#task1
+# var1 = int(input("Enter number 1: "));
+# var2 = int(input ("Enter number 2:"));
+# print(f"The sum of two nums is {var1+var2}. \n");
+
+
+#task2
+# list = ["apple", "mangoes", "23"];
+# for i in range(len(list)):
+#     str = list[i];
+#     print(str[0]);
+
